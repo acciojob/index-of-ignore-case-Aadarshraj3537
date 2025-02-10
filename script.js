2ebb34b4a11f6cae7fde921100e78db7be50bb40
Aadarsh Raj <aadarshraj351@gmail.com>
2025-02-10 11:08:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function indexOfIgnoreCase(s1, s2) {
-  if (!s1 || !s2) return -1; // Handle empty/null inputs
+  if (!s1 || !s2) return -1; 
   return s1.toLowerCase().indexOf(s2.toLowerCase());
 }
-
-// Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
